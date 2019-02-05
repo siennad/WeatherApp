fa7e905f70f3629e8ff7827ac06074684a719cec
siennad <siennad97@gmail.com>
2019-02-05 15:13:42 +0200

    Edit search bar to search smooth and work on Android

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -9,26 +9,28 @@ import SearchIcon from '@material-ui/icons/Search';
 
 import SearchResult from './SearchResult';
 
-const CITY_URL = 'dataList/cityList.json'
+const CITY_URL = 'dataList/cityList.json';
+
+const cities = require('../../www/dataList/cityList.json');
 
 const styles = theme => ({
-    formWrap: {
-        padding: '2px 4px',
-        display: 'flex',
-        alignItems: 'center',
-        width: "100%",
-        margin: "auto"
-    },
-    container: {
-        display: 'flex',
-        flexWrap: 'wrap',
-    },
-    textField: {
-        margin: theme.spacing.unit,
-    },
-    iconButton: {
-        padding: 10,
-      },
+  formWrap: {
+    padding: '2px 4px',
+    display: 'flex',
+    alignItems: 'center',
+    width: '100%',
+    margin: 'auto'
+  },
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap'
+  },
+  textField: {
+    margin: theme.spacing.unit
+  },
+  iconButton: {
+    padding: 10
+  }
 });
 
 class SearchBar extends Component {
@@ -38,73 +40,78 @@ class SearchBar extends Component {
       val: '',
       searchResult: [],
       isFetching: false
-    }
-    this.handleChangeSearchBox = this.handleChangeSearchBox.bind(this);   
-    this.searchBtnClicked = this.searchBtnClicked.bind(this);   
+    };
+    this.handleChangeSearchBox = this.handleChangeSearchBox.bind(this);
+    this.searchBtnClicked = this.searchBtnClicked.bind(this);
     this.searchLocation = this.searchLocation.bind(this);
-  }   
-
-  searchLocation(val) {
-    fetch(`${CITY_URL}`)
-      .then(res => res.json())
-      .then(respond => {
-        let dataToReturn;
-        dataToReturn = respond.filter(res => res.name.toLowerCase().startsWith(val.toLowerCase().trim()))
-        dataToReturn = (dataToReturn.length >= 5) ? dataToReturn : [...dataToReturn, respond.filter(res => res.country.toLowerCase().include(val.toLowerCase()))]
-        return dataToReturn.slice(0,5)
-      })
-      .then((res) => {
-        const data = res.map((d) => ({
-          name: d.name,
-          country: d.country,
-          id: d.id
-          })
-        )
-        this.setState({
-          ...this.state,
-          searchResult: data,
-          isFetching: false
-        })
-      })
-    
+    this.delay = this.delay.bind(this);
   }
 
-  handleChangeSearchBox(event) {
+  async delay(sec) {
+    return new Promise(resolve => {
+      setTimeout(resolve, sec*1000)
+    })
+  }
+
+  async searchLocation(val) {
+    let dataToReturn;
+    dataToReturn = await cities.filter(res =>
+      res.name.toLowerCase().startsWith(val.toLowerCase().trim())
+    );
+    dataToReturn = (await (dataToReturn.length < 5 && val.length <= 2))
+      ? [
+          ...dataToReturn,
+          cities.filter(res =>
+            res.country.toLowerCase().includes(val.toLowerCase())
+          )
+        ].slice(0, 5) : dataToReturn.slice(0,5);
+    let data;
+    data = await dataToReturn.map(d => ({
+      name: d.name,
+      country: d.country,
+      id: d.id
+    }));
+    await this.setState({
+      searchResult: data,
+      isFetching: false
+    });
+  }
+
+  async handleChangeSearchBox(event) {
     let val = event.target.value;
     this.setState({
       ...this.state,
       val: val,
       isFetching: true
-    })
+    });
+    await this.delay(0.5);
     if (val && val.length > 0) {
-      this.searchLocation(val) ;
+      await this.searchLocation(val);
     } else {
-      this.setState({
-        input: '',
+      await this.setState({
         searchResult: [],
         isFetching: false
-      })
+      });
     }
   }
 
-  searchBtnClicked() {     
+  searchBtnClicked() {
     this.setState({
       ...this.state,
-      isFetching: true,
-    })
+      isFetching: true
+    });
 
-    if (val && val.length > 0) {
+    if (this.state.val && this.state.val.length > 0) {
       this.searchLocation(this.state.val);
     } else {
       this.setState({
-        input: '',
         searchResult: [],
         isFetching: false
-      })
+      });
     }
   }
 
-  render () {
+  render() {
     const { classes } = this.props;
 
     return (
@@ -114,30 +121,34 @@ class SearchBar extends Component {
             <TextField
               id="standard-with-placeholder"
               label="Search"
-              placeholder='City, country...'
+              placeholder="City, country..."
               fullWidth={true}
               className={classes.textField}
               margin="normal"
               variant="outlined"
-              value={this.state.val} 
+              value={this.state.val}
               onChange={this.handleChangeSearchBox}
             />
-            <IconButton 
-              className={classes.iconButton} 
+            <IconButton
+              className={classes.iconButton}
               aria-label="Search"
-              onClick={this.searchBtnClicked}>
+              onClick={this.searchBtnClicked}
+            >
               <SearchIcon />
             </IconButton>
           </Paper>
         </form>
-        <SearchResult isFetching={this.state.isFetching} list={this.state.searchResult} />
-      </div>            
-    )
+        <SearchResult
+          isFetching={this.state.isFetching}
+          list={this.state.searchResult}
+        />
+      </div>
+    );
   }
 }
 
 SearchBar.propTypes = {
-    classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(SearchBar);
